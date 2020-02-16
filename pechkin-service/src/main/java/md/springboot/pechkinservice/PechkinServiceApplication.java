aca2afe0cb1255dbb70451e1c02586aaf6057fd0
vadim eladii <vadimeladii@gmail.com>
2020-02-16 23:11:46 +0200

    Add pechkin-service module

---
+++ pechkin-service/src/main/java/md/springboot/pechkinservice/PechkinServiceApplication.java
@@ -0,0 +1,13 @@
+package md.springboot.pechkinservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PechkinServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PechkinServiceApplication.class, args);
+    }
+
+}
---
+++ pechkin-service/src/test/java/md/springboot/pechkinservice/PechkinServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package md.springboot.pechkinservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PechkinServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
