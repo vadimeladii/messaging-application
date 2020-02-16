4ed775efcd0111ff7bfb4338c049450c0cd4fa15
vadim eladii <vadimeladii@gmail.com>
2020-02-16 23:29:02 +0200

    Add agent-smith-service module

---
+++ agent-smith-service/src/main/java/md/springboot/AgentSmithServiceApplication.java
@@ -0,0 +1,13 @@
+package md.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentSmithServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgentSmithServiceApplication.class, args);
+    }
+
+}
